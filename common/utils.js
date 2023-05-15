4ea8eac316891f799a164abb8c2f9f9903cc03f7
S.Ravi Shankar <sankarsana@gmail.com>
2023-05-15 18:26:00 +0530

    15-May-version

--- common/utils.js
+++ common/utils.js
@@ -8,5 +8,18 @@ utils.printProgress = (count, max) => {
     const percent = utils.formatPercent(count / max);
     process.stdout.write(count + "/" + max + " (" + percent + ")");
 }
+
+utils.groupBy = (objArray, key) => {
+    const groups = {};
+    for (let obj of objArray) {
+        const val = obj[key];
+        if (groups[val] == null) {
+            groups[val] = [];
+
+        }
+        groups[val].push(obj);
+    }
+    return groups;
+}
 if (typeof module !== 'undefined')
     module.exports = utils;
