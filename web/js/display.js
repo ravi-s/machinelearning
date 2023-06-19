4daee5c01b6d9bc45fa53644f766a26f94e95de8
S.Ravi Shankar <sankarsana@gmail.com>
2023-06-19 11:12:55 +0530

    19-June_11am

--- web/js/display.js
+++ web/js/display.js
@@ -59,3 +59,11 @@ function handleClick(sample, doScroll = true) {
     }
     chart.selectSample(sample)
 }
+function toggleInput() {
+    if (inputContainer.style.display == "none") {
+        inputContainer.style.display = 'block';
+    }
+    else {
+        inputContainer.style.display = 'none';
+    }
+}
